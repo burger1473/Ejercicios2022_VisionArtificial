af669bf1280d69044697ae538b34e0ac6ebbec04
burger1473 <fabianburgos.09@gmail.com>
2022-05-05 17:39:23 -0300

    Cambio TP9

--- Practico9_2.py
+++ Practico9_2.py
@@ -1,12 +1,9 @@
 '''/*=============================================================================
  * Author: Fabian Burgos
  * Date: 18/04/2022
- * Rev:  05/05/2022
  * Version: Python 3.7.0
  *          Open CV: 4.5.4-dev
- * Descripcion: Se usa una sola imagen en donde hay algún objeto que sufrio una 
- *              transformacion perspectiva y el objetivo es que ese objeto vuelva 
- *              a su forma original.
+ * Descripcion: Rectificando imágenes
  *===========================================================================*/'''
 
 #======================== Incluciones ====================================
@@ -17,8 +14,6 @@ from tkinter import filedialog as filedialog    #Para abrir y guardar archivos
 import numpy as np                              #Para tratar con numeros matematicos para opencv    
 import cv2                                      #Librerira opencv
 import copy                                     #Para poder copiar matrices
-import os                                       #Para operaciones del sistema  
-import math                                     #Para calculos matematicos
 
 #======================== Variable ====================================
 Nombre_app="Practico 9 - Burgos"
@@ -26,16 +21,9 @@ ubicacion=""
 img = np.zeros((512, 512, 3),np.uint8)
 img_mod = np.zeros((512, 512, 3),np.uint8)
 img_dos = np.zeros((512, 512, 3),np.uint8)
-img_transformada = np.zeros((512, 512, 3),np.uint8)
-img_transformada_mod = np.zeros((512, 512, 3),np.uint8)
 cant_puntos = 0
 puntos = [[0,0],[0,0],[0,0],[0,0]]
-drawing = False # true if mouse is pressed
-mode = True # if True, draw rectangle. Press ’m’ to toggle to curve
-ix= -1
-iy= -1
-fx= -1
-fy= -1
+
 #======================== Implementaciones=============================
 
 '''/*========================================================================
@@ -57,49 +45,60 @@ def Keyboardpress( key):
 
 '''/*========================================================================
 Funcion: transformación
-Descripcion: Realiza el rectificado de una imagen
+Descripcion: Realiza el rectificado de una imagen y la incrusta en otra
 Sin parametro de entrada
 No retorna nada
 ========================================================================*/'''
 def transformacion():
-    global img, imagen_ancho, imagen_alto, img_transformada
+    global img, img_dos, img_mod
+    rows, cols = img_dos.shape[:2]  #Tamaño de la imagen a transformar e incrustar
     rows2, cols2 = img.shape[:2]    #Tamaño de la imagen original
 
     #Defino los 4 pares de puntos correspondientes
-    input_pts = np.float32([[puntos[0][0],puntos[0][1]], [puntos[1][0],puntos[1][1]], [puntos[2][0],puntos[2][1]], [puntos[3][0],puntos[3][1]]])
-    output_pts = np.float32([[0,0], [imagen_ancho.get(),0], [imagen_ancho.get(),imagen_alto.get()], [0,imagen_alto.get()]])
-
+    input_pts = np.float32([[0,0], [cols,0], [cols,rows], [0,rows]])
+    output_pts = np.float32([[puntos[0][0],puntos[0][1]], [puntos[1][0],puntos[1][1]], [puntos[2][0],puntos[2][1]], [puntos[3][0],puntos[3][1]]])
+    
     #Calcule la matriz de transformación usando cv2.getPerspectiveTransfor()
     M= cv2.getPerspectiveTransform(input_pts , output_pts)
+    
     #Aplico la transformación afín usando cv2.warpAffine()
-    img_transformada = cv2.warpPerspective(img, M, (cols2,rows2),flags=cv2.INTER_LINEAR)
+    dst = cv2.warpPerspective(img_dos, M, (cols2,rows2))
+    
+    cambiar_texto_label2("Por favor espere...", "blue")  #Cambio texto y color de label2
 
+    #Realizo mascara e incruso la imagen
+    for row in range (dst.shape[1]):                    #Recorro el ancho de la imagen transformada
+        for col in range (dst.shape[0]):                #Recorro el largo de la imagen transformada
+            if (dst[col][row] != 0).all():              #Si el pixel no es el color negro
+                img_mod[col][row]=dst[col][row]         #Remplazo el pixel de la foto original con el pixel de la foto transformada
     
-    
-    cv2.imshow('Resultado', img_transformada)                     #Muestro el resultado
-    cambiar_texto_label2("Proceso finalizado", "green")           #Cambio texto y color de label2
+    cv2.imshow('Resultado', img_mod)                    #Muestro el resultado
+    cambiar_texto_label2("Proceso finalizado", "green") #Cambio texto y color de label2
 
 
 '''/*========================================================================
 Funcion: callback 
-Descripcion: Funcion a la cual se llama por algun evento producido por el mause en la figura 1
+Descripcion: Funcion a la cual se llama por algun evento producido por el mause
 Sin parametro de entrada
 No retorna nada
 ========================================================================*/'''
 def callback ( event , x , y , flags , param):
-    global cant_puntos, img_mod
+    global img, cant_puntos, img_dos
     if event == cv2.EVENT_LBUTTONDOWN:                                     #Si el boton izquierdo se presiona
         if cant_puntos<4:
-            cv2.circle(img_mod, (x,y) , 3 , (0,0,255) , -1)                    #Dibujo punto
+            cv2.circle(img, (x,y) , 3 , (0,0,255) , -1)                    #Dibujo punto
             puntos[cant_puntos][0]=x                                       #Almaceno la posicion x del punto
             puntos[cant_puntos][1]=y                                       #Almaceno la posicion y del punto
             cant_puntos=cant_puntos+1
     elif event == cv2.EVENT_LBUTTONUP:                                     #Si el boton izquierdo fue levantado
         if cant_puntos == 4:
+            cambiar_texto_label2("Seleccionar imagen a incrustar", "red")  #Cambio texto y color de label2
+            ubicacion = filedialog.askopenfilename(title='Seleccionar imagen a incrustar',initialdir='/', filetypes=(('Archivo png', '*.png*'),('Archivo jpg', '*.jpg'))) #Obtengo la ruta seleccionada
+            img_dos = cv2.imread(ubicacion , cv2.IMREAD_COLOR)             #Obtengo la imagen de la ubicacion seleccionada
             cant_puntos=5                                                  #Valorizo en 5 para que cualquier click izquierdo no tenga efecto
             transformacion()
-    cv2.imshow('Original', img_mod)                                 #Muestro imagen en la ventana
-    
+
+    cv2.imshow('Original', img)                                            #Refresco muestra de imagen con recuadro
 
 
 '''/*========================================================================
@@ -117,8 +116,8 @@ def Abrir_foto():
     cv2.namedWindow('Original')                                 #Indico nombre de la ventana
     cv2.setMouseCallback ('Original',callback)                  #Establesco evento de mause sobre la ventana
     print("Foto abierta")                                       #Indico que termino el proceso por la consola
+    cv2.imshow('Original', img)                                 #Muestro imagen en la ventana
     img_mod=copy.deepcopy(img)                                  #Copio la imagen original en una variable
-    cv2.imshow('Original', img_mod)                                 #Muestro imagen en la ventana
     cant_puntos=0                                               #Reseteo la cant de puntos cuando se abre una nueva imagen
 
 '''/*========================================================================
@@ -128,11 +127,11 @@ Sin parametro de entrada
 No retorna nada
 ========================================================================*/'''
 def Guardar_imagen():
-    global img_transformada                                     #Obtengo las variables globales
+    global img_mod                                             #Obtengo las variables globales
     directorio=filedialog.asksaveasfilename(initialdir = "/",title = "Guardar como",filetypes = (('Archivo png', '.png'),('Archivo jpg', '.jpg'),("todos los archivos","*.*")),defaultextension='.png')  #Abro ventana para seleccionar ubicacion
-    print("Ubicacion imagen: ")             
+    print("Ubicacion imagen recotada: ")             
     print(directorio)
-    cv2.imwrite( directorio, img_transformada)                  #Guardo la imagen binaria o procesada
+    cv2.imwrite( directorio, img_mod)                           #Guardo la imagen binaria o procesada
     #showinfo.showinfo("Practico2", "Guardado con exito")       # título, mensaje
     cambiar_texto_label2("Guardado con exito", "black")         #Cambio texto y color de label2
 
@@ -155,15 +154,12 @@ def cambiar_texto_label2(texto, color):
 
 # Creo el root de windows
 root = tk.Tk()
-imagen_alto = DoubleVar()
-imagen_ancho = DoubleVar()
-imagen_alto.set(50)
-imagen_ancho.set(100)
+
 #bit = root.iconbitmap('icon.ico')
 root.title(Nombre_app)
 root.resizable(False, False)
-root.geometry('300x300')
-
+root.geometry('300x140')
+barra = DoubleVar()
 
 #Describo boton abrir archivo
 open_button = ttk.Button(
@@ -184,11 +180,6 @@ guardar_button = ttk.Button(
 #Implemento los botones en el root
 open_button.pack(side=tk.TOP, fill=tk.BOTH, padx=5, pady=5)
 guardar_button.pack(side=tk.TOP, fill=tk.BOTH, padx=5, pady=5)
-
-
-s2 = tk.Scale(root, variable = imagen_alto, from_=0, to=100, tickinterval=20, resolution =   0.1, orient=tk.HORIZONTAL, length=300, label = "Alto pixel: ").pack()
-s3 = tk.Scale(root, variable = imagen_ancho, from_=0, to=100, tickinterval=20, resolution =   0.1, orient=tk.HORIZONTAL, length=300, label = "Ancho pixel: ").pack()
-
 
 #genero un label
 label1 = Label(root,text="Para resetear, vuelva a cargar una imagen")
